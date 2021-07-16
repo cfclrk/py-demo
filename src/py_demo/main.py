333c9d5b6734c2b757f69b3da3eeddab364ccbff
Chris Clark <cfclrk@gmail.com>
2021-07-16 08:51:58 -0400

    Add comments in main.py

--- src/py_demo/main.py
+++ src/py_demo/main.py
@@ -7,7 +7,7 @@ from py_demo import __summary__, __version__, data_files
 
 
 def cli() -> None:
-    """Parse command line arguments and pass them to ``main``.
+    """Parse command line arguments and call ``main``.
 
     This is the interactive (CLI) entry-point to the program.
     """
@@ -27,10 +27,11 @@ def cli() -> None:
 
 
 def main(opts: dict) -> dict:
-    """Run the program with the given options.
+    """Run this program with the given options.
 
-    This is the API entry-point to the program. Other projects should be able to import
-    this project and run this function with the same options that the CLI supports.
+    This is the programmatic entry-point to the program. Python projects should be able
+    to import this package and run this function using the same options that the CLI
+    supports.
     """
     version = f"{sys.version_info.major}.{sys.version_info.minor}"
     data = resources.read_text(data_files, "some_data.txt").strip()
@@ -38,5 +39,6 @@ def main(opts: dict) -> dict:
     return {"opts": opts, "version": version, "data": data, "foo": foo}
 
 
+# When executed interactively (vs being programmatically imported), use the CLI.
 if __name__ == "__main__":
     cli()
